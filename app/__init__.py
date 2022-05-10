8ae7f3b58aa131e8ff429c73904235782a95a166
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-10 13:35:41 +0300

    Create app configurations.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from config import config_options
 
 
-def create_app(congig_name):
+def create_app(config_name):
   app = Flask(__name__)
+
+  #Creating app configurations
+  app.config.from_object(config_options[config_name])
 
   #Registering the blueprint
   from . main import main as main_blueprint
   app.register_blueprint(main_blueprint)
 
+  return app
 
 
