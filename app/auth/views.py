2fcfe77219ff3a4a39cab22441a5794ea66ad88f
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-11 02:06:58 +0300

    add a function to log out the user and redirect them to the index page.

--- app/auth/views.py
+++ app/auth/views.py
@@ -36,7 +36,11 @@ def register():
     return render_template('auth/register.html',registration_form = form,title=title)
 
 
-
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
 
 
 
