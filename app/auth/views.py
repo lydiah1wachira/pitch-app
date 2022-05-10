24017551cb29d3cb3ce519be19ceb703c81f1f6b
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-11 02:05:50 +0300

    fix bug in the registration route.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,url_for,flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from . import auth
 from ..models import User
 from .forms import SignUpForm,LoginForm
@@ -30,8 +30,13 @@ def register():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
+
+    title = "New Account"
+
+    return render_template('auth/register.html',registration_form = form,title=title)
+
+
+
 
 
 
