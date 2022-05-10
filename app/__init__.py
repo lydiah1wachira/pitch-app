08e341b2be72a08c734935a870cffdb97b1ec8be
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-10 13:30:16 +0300

    Register the blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,9 @@ from flask import Flask
 def create_app(congig_name):
   app = Flask(__name__)
 
+  #Registering the blueprint
+  from . main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+
 
