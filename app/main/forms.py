ff4a98d2ae46ee0138077e460edab802e2fd42ee
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-11 03:05:50 +0300

    create an Update-profile form classto update the bio.

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,7 @@ class CommentsForm(FlaskForm):
 
     comments = TextAreaField('Movie review', validators=[Required()])
     submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
