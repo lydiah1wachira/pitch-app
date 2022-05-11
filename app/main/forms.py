8e041d77d636fb220c1d5ba7d15d3345da50a9a5
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-11 22:04:42 +0300

    create a comments form.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField,TextAreaField,SubmitField, SelectField
+from wtforms.validators import DataRequired, ValidationError
+from flask_login import current_user
 
-class CommentsForm(FlaskForm):
+class PitchForm(FlaskForm):
+  
+    content = TextAreaField('Content',validators=[DataRequired()])
 
+    category = SelectField('Category', choices=[('General', 'General'),('Business','Business'),('Career','Career')],validators=[DataRequired()])
 
-    comments = TextAreaField('Movie review', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField('SubmitPitch')
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
     submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    
+    comment = TextAreaField('Comments...',validators=[DataRequired()])
+    submit = SubmitField('comment')
