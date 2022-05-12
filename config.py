dd114d88cabc9f01b9ef2d96f638d4c662b19497
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-12 11:42:55 +0300

    deployment to heroku.

--- config.py
+++ config.py
@@ -4,16 +4,18 @@ class Config:
   '''
   General configuration parent class
   '''
-  SECRET_KEY = '1b@.]NNXr;N$j=brBcNrj#BXMZb$|S'
+  SECRET_KEY = 'k9YIw3u'
   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wachira:Lydiah007@localhost/pitches'
   UPLOADED_PHOTOS_DEST ='app/static/photos'
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  SQLALCHEMY_TRACK_MODIFICATIONS=True
 
   #  email configurations
   MAIL_SERVER = 'smtp.googlemail.com'
   MAIL_PORT = 587
   MAIL_USE_TLS = True
   MAIL_USERNAME='oneminutepitches@gmail.com'
-  MAIL_PASSWORD='N*WC&5tt'
+  MAIL_PASSWORD='wakadinali'
 
 
 
